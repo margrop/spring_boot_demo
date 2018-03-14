bee84dc7368c15bc86d47bf36a7d494d0c86ad91
xucheng <xucheng@yme.me>
2018-03-14 14:09:01 +0800

    availableWorkersOfShip step 1

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,12 +1,13 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/demo")
 public class DemoController {
-    @RequestMapping("/hello.do")
+    @GetMapping("/hello.do")
     public String hello() {
         return "hello";
     }
